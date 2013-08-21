07c567b7174eab7dbdbd514c34af40b652729e15
Orlando <orlando_av16@hotmail.com>
2013-08-21 18:22:37 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedSocial::Application.config.secret_token = 'f0da954601dc9ab263689a407f7190e735d925bd713b1b4fcc4eb3377df35a1b45b17901382ae900f16aa7d82e43d3ebb73b8c24aa1279b64ba3b1ed0cd9aebc'
